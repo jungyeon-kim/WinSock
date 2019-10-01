7bce257c39d703090ca2b5eacd04f239b1b04d05
Kim Jung Yeon <zoemfhs123@gmail.com>
2019-10-01 11:05:10 +0900

    고정길이 데이터 전송
    버퍼사이즈를 50으로 고정시키고 문자열의 남은 버퍼를 #으로 채운뒤 서버로 전송

--- Client/Client/6.TCPClient.cpp
+++ Client/Client/6.TCPClient.cpp
@@ -48,6 +48,8 @@ int recvn(SOCKET s, char *buf, int len, int flags)
     char *ptr = buf;
     int left = len;
 
+
+
     while (left > 0) {
         received = recv(s, ptr, left, flags);
         if (received == SOCKET_ERROR)
